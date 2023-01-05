193fcf30634d585443c7f17a40a3275c1f9d692f
Farid Adam <faridibin@gmail.com>
2023-01-04 22:54:01 -0500

    factories

---
+++ database/factories/FeatureFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Faridibin\Laraflags\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class FeatureFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \Faridibin\Laraflags\Models\Features::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->sentence,
+            'active' => true,
+            'expires_at' => null,
+        ];
+    }
+}
