eda2d56b173ba4d040618f2bb7987ba819cd0d10
Farid Adam <faridibin@gmail.com>
2023-01-09 01:22:29 -0500

    Create TestCase.php

---
+++ tests/TestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Faridibin\Laraflags\Tests;
+
+use Faridibin\Laraflags\LaraflagsServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withFactories(__DIR__ . '/database/factories');
+    }
+
+    /**
+     * Define package service providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaraflagsServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testing_db');
+        $app['config']->set('database.connections.testing_db', [
+            'driver' => 'sqlite',
+            'database' => ':memory:'
+        ]);
+    }
+}
