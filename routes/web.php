6a565ddc469e75e3dc55a67f069e4a5251de723b
Farid Adam <faridibin@gmail.com>
2023-01-09 01:21:50 -0500

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where we register web routes for Laraflags. These routes are loaded
+| by the RouteServiceProvider within a group which contains
+| the "web" middleware group.
+|
+*/
