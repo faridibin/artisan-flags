1852a475661ff8102e17571b61f274e1588b8b3e
Farid Adam <faridibin@gmail.com>
2023-01-14 23:33:29 -0500

    Create FeatureGroupsRepository.php

---
+++ src/Repositories/FeatureGroupsRepository.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Faridibin\Laraflags\Repositories;
+
+use Faridibin\Laraflags\Interfaces\Repositories\LaraflagsRepositoryInterface;
+use Faridibin\Laraflags\Models\FeatureGroups;
+
+class FeatureGroupsRepository implements LaraflagsRepositoryInterface
+{
+    /**
+     * The feature group model instance.
+     *
+     * @var \Faridibin\Laraflags\Models\FeatureGroups
+     */
+    protected $model;
+
+    /**
+     * Create a new repository instance.
+     */
+    public function __construct()
+    {
+        $this->model = new FeatureGroups();
+    }
+
+    /**
+     * Gets a feature group by id.
+     *
+     * @param int $id
+     * @return FeatureGroups|null
+     */
+    public function getById(int $id): ?FeatureGroups
+    {
+        return $this->model->find($id);
+    }
+
+    /**
+     * Gets a feature group by name.
+     *
+     * @param string $name
+     * @return FeatureGroups|null
+     */
+    public function getByName(string $name): ?FeatureGroups
+    {
+        return $this->model->where('name', $name)->first();
+    }
+
+    /**
+     * Creates a new feature group.
+     *
+     * @param array<string, mixed> $data
+     * @return FeatureGroups
+     */
+    public function create(array $data): FeatureGroups
+    {
+        return $this->model->create($data);
+    }
+
+    /**
+     * Updates a feature.
+     *
+     * @param int $id
+     * @param array<string, mixed> $data
+     * @return FeatureGroups
+     */
+    public function update(int $id, array $data): FeatureGroups
+    {
+        $featureGroup = $this->model->find($id);
+        $featureGroup->update($data);
+
+        return $featureGroup;
+    }
+
+    /**
+     * Deletes a feature.
+     *
+     * @param int $id
+     * @return FeatureGroups
+     */
+    public function delete(int $id): FeatureGroups
+    {
+        $featureGroup = $this->model->find($id);
+        $featureGroup->delete();
+
+        return $featureGroup;
+    }
+}
