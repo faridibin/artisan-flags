94a33e7654c049c839166ae75969dc15442bfc99
Farid Adam <faridibin@gmail.com>
2023-01-14 23:32:21 -0500

    Create AttachToGroup.php

---
+++ src/Console/Feature/AttachToGroup.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Faridibin\Laraflags\Console\Feature;
+
+use Faridibin\Laraflags\Facades\Laraflags;
+use Faridibin\Laraflags\Models\Features;
+use Faridibin\Laraflags\Models\FeatureGroups;
+use Faridibin\Laraflags\Console\Traits\CreatesGroup;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+
+class AttachToGroup extends Command
+{
+    use CreatesGroup;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laraflags:attach-to-group {feature?} {--g|group=*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Attaches a feature flag to a feature group';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $feature = $this->handleGetFeaturePrompt();
+        $groups = $this->handleGetFeatureGroupPrompt();
+
+        if ($groups->isNotEmpty()) {
+            $groups->each(function ($group) use ($feature) {
+                if ($group instanceof FeatureGroups) {
+                    $feature->groups()->attach($group);
+
+                    $this->comment("The feature [{$feature->name}] was attached to feature group [{$group->name}]!");
+                } else {
+                    $this->info("The feature group option [{$group}] was skipped.");
+                }
+            });
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * Prompts the user for a feature name.
+     *
+     * @return Features
+     */
+    private function handleGetFeaturePrompt(): Features
+    {
+        $name = $this->argument('feature');
+
+        if (!$name) {
+            $name = $this->ask('Please enter the name of the feature');
+        }
+
+        $feature = Laraflags::feature($name);
+
+        while (!$feature) {
+            $this->alert("A feature does not exist with this name [{$name}]");
+            $name = $this->ask('Please enter the name of the feature');
+
+            $feature = Laraflags::feature($name);
+        }
+
+        return $feature;
+    }
+
+    /**
+     * Prompts the user for a feature group name.
+     *
+     * @return Collection
+     */
+    private function handleGetFeatureGroupPrompt(): Collection
+    {
+        $groups = collect($this->option('group'));
+
+        if ($groups->isEmpty()) {
+            $name = null;
+
+            while (!$name) {
+                $name = $this->ask('Please enter the name of the feature group to attach');
+            }
+
+            $groups->push($name);
+        }
+
+        return $groups->map(function ($name) {
+            $group = Laraflags::group($name);
+
+            if (!$group) {
+                $this->info("A feature group [{$name}] does not exist.");
+                $creates = $this->choice("Do you want to create a feature group with this name [{$name}]?", ['no', 'yes']);
+
+                if ($creates === 'yes') {
+                    $group = $this->createGroup($name);
+                }
+            }
+
+            return $group;
+        });
+    }
+}
