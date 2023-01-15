701a3b0c8e0b85d1f24cda1876cbc9fe82db9ea2
Farid Adam <faridibin@gmail.com>
2023-01-14 23:32:36 -0500

    Create DetachFeatureFromTenant.php

---
+++ src/Console/Feature/DetachFeatureFromTenant.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Console\Commands\Feature;
+
+use App\Models\Feature\Features;
+use App\Traits\Console\FeatureFlags;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+
+class DetachFeatureFromTenant extends Command
+{
+    // use FeatureFlags;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laraflags:detach-to-tenant {feature?} {--t|tenant=*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Detaches a feature flag to a tenant';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // if (!config('feature-flags.tenancy.enabled', false)) {
+        //     $this->error('Feature-flags do not have tenancy enabled!');
+
+        //     return Command::FAILURE;
+        // }
+
+        // $feature = $this->handleFeaturePrompt();
+        // $tenants = $this->handleTenantPrompt();
+
+        // foreach ($tenants['found'] as $tenant) {
+        //     $identifier = config('feature-flags.tenancy.identifier');
+        //     if ($feature->hasTenant($tenant->name)) {
+        //         $this->comment("The feature [{$feature->name}] is already detached to tenant [{$tenant->$identifier}]!");
+
+        //         continue;
+        //     }
+
+        //     $feature->tenants()->detach($tenant);
+
+        //     $this->comment("The feature [{$feature->name}] was detached to tenant [{$tenant->$identifier}]!");
+        // }
+
+        // if (count($tenants['notFound']) > 0) {
+        //     $notFound = Arr::join($tenants['notFound'], ', ', ' and ');
+
+        //     $this->alert("These tenants do not exist! [{$notFound}]");
+        // }
+
+        return Command::SUCCESS;
+    }
+
+    // /**
+    //  * Prompts the user for a feature name.
+    //  *
+    //  * @return Features
+    //  */
+    // private function handleFeaturePrompt(): Features
+    // {
+    //     $feature = $this->argument('feature');
+
+    //     if (!$feature) {
+    //         $feature = $this->ask('Please enter the name of the feature');
+    //     }
+
+    //     $featureExists = $this->doesFeatureExist($feature);
+
+    //     while (!$featureExists) {
+    //         $this->alert("A feature does not exist with this name [{$feature}]");
+    //         $feature = $this->ask('Please enter a name of the feature');
+
+    //         $featureExists = $this->doesFeatureExist($feature);
+    //     }
+
+    //     return $featureExists;
+    // }
+
+    // /**
+    //  * Prompts the user for a tenant name.
+    //  *
+    //  * @return array
+    //  */
+    // private function handleTenantPrompt(): array
+    // {
+    //     $data = [
+    //         'found' => [],
+    //         'notFound' => [],
+    //     ];
+
+    //     $tenants = $this->option('tenant');
+
+    //     if (!$tenants) {
+    //         $tenants[] = $this->ask('Please enter the name of the tenant');
+    //     }
+
+    //     foreach ($tenants as $tenant) {
+    //         $tenantExists = $this->doesTenantExist($tenant);
+
+    //         if ($tenantExists) {
+    //             $data['found'][] = $tenantExists;
+
+    //             continue;
+    //         }
+
+    //         $data['notFound'][] = $tenant;
+    //     }
+
+    //     return $data;
+    // }
+}
